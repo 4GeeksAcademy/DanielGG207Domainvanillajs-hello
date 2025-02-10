4ae055b29ab8a00ae6885c93dc2f60a948bb2a7d
DanielGG207 <cocorroquiasprogamer@gmail.com>
2025-02-10 23:29:45 +0000

    [Proyecto Dominio terminado]
    Imprime dominios con un easter egg the last of Us

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function domgen (pronouns, adj , noun, ) {
+  
+  let domain = ""
+ for(let i = 0; i < pronouns.length ; i++) {
+ 
+   for (let j = 0; j < adj.length ; j++) {
+    
+    for (let h = 0; h < noun.length ; h++){
+      domain = pronouns[i] + adj [j] + noun[h] 
+      if (domain == "thelastof")
+       console.log(domain + ".us")
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+      else{
+        console.log(domain + ".com")
+      }
+
+
+    }
+   }
+
+ }
+}
+
+ let answer = domgen (['the', 'our'], ['great', 'big', "last"], ['jogger', 'racoon', "of"])
